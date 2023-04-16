bba9064c4279680ebff1783d5fbefd2475233802
elliot-goodman <goodman.el@northeastern.edu>
2023-04-16 19:59:56 -0400

    added a few more routes to employees, it now has 8. Still haven't tested (:

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -4,11 +4,150 @@ from src import db
 
 employees = Blueprint('employees', __name__)
 
+# Create a new employee
+@employees.route('/employees/<employee_id>/', methods=['POST'])
+def create_employee(employee_id):
+    data = request.json
+    
+    employee_id = data["employee_id"]
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    country = data["country"]
+    city = data["city"]
+    state = data["state"]
+    zip_code = data["zip_code"]
+    gender = data["gender"]
+    date_of_birth = data["date_of_birth"]
+    salary = data["salary"]
+    project_id = data["project_id"]
+    is_remote = data["is_remote"]
+    supervisor_id = data["supervisor_id"]
+    team_id = data["team_id"]
+    
+    query = 'insert into Employees (employee_id, first_name, last_name, country, city, state, zip_code, gender, date_of_birth, salary, project_id, is_remote, supervisor_id, team_id) values ("'
+    query += str(employee_id) + '",'
+    query += first_name + ', "'
+    query += last_name + '", '
+    query += country + '",'
+    query += state + '",'
+    query += zip_code + ', "'
+    query += gender + '", '
+    query += str(date_of_birth) + '",'
+    query += salary + '",'
+    query += str(project_id) + ', "'
+    query += str(is_remote) + '", '
+    query += str(supervisor_id) + '",'
+    query += str(team_id) + ')'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return ("Success")
+
+# Update a particular employee
+@employees.route('/employees/<employee_id>/', methods=['PUT'])
+def update_employee(employee_id):
+    data = request.json
+    
+    employee_id = data["employee_id"]
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    country = data["country"]
+    city = data["city"]
+    state = data["state"]
+    zip_code = data["zip_code"]
+    gender = data["gender"]
+    date_of_birth = data["date_of_birth"]
+    salary = data["salary"]
+    project_id = data["project_id"]
+    is_remote = data["is_remote"]
+    supervisor_id = data["supervisor_id"]
+    team_id = data["team_id"]
+
+    query = 'update Employees set '
+    query += str(employee_id) + '",'
+    query += first_name + ', "'
+    query += last_name + '", '
+    query += country + '",'
+    query += state + '",'
+    query += zip_code + ', "'
+    query += gender + '", '
+    query += str(date_of_birth) + '",'
+    query += salary + '",'
+    query += str(project_id) + ', "'
+    query += str(is_remote) + '", '
+    query += str(supervisor_id) + '",'
+    query += str(team_id) + ')'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return ("Success")
+
+# Delete a particular employee
+@employees.route('/employees/<employees_id>/', methods=['DELETE'])
+def employee(employee_id):
+    
+    query = 'delete from Employees where employee_id = {0}'.format(employee_id)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return ("Success")
+
 # Get all details about an employee with a specific employee_id
 @employees.route('/employees/<employee_id>', methods=['GET'])
 def get_employee_detail(employee_id):
     cursor = db.get_db().cursor()
     cursor.execute('select * from Employees where employee_id = {0}'.format(employee_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all employees (ID) working on a specific project
+@employees.route('/employees/<employee_id>/', methods=['GET'])
+def get_employees_on_project(project_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select employee_id from Employees where project_id = {0}'.format(project_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all employees (ID) in a specific team
+@employees.route('/employees/<employee_id>/', methods=['GET'])
+def get_employees_on_team(team_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select employee_id from Employees where team_id = {0}'.format(team_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all employees (ID) that are either remote or not
+@employees.route('/employees/<employee_id>/', methods=['GET'])
+def get_employee_remote(is_remote):
+    cursor = db.get_db().cursor()
+    cursor.execute('select employee_id from Employees where is_remote = {0}'.format(is_remote))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -33,3 +172,4 @@ def get_employee_project(employee_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
