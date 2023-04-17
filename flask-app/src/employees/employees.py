5583ac2f447dee3ebe3ff0ff92744b16e2da9b3e
elliot-goodman <goodman.el@northeastern.edu>
2023-04-16 20:55:05 -0400

    added blueprint for employees, and added 1 more route to retrive all employees

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -99,10 +99,25 @@ def employee(employee_id):
     return ("Success")
 
 # Get all details about an employee with a specific employee_id
-@employees.route('/employees/<employee_id>', methods=['GET'])
+@employees.route('/employees/<employee_id>/', methods=['GET'])
 def get_employee_detail(employee_id):
     cursor = db.get_db().cursor()
     cursor.execute('select * from Employees where employee_id = {0}'.format(employee_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all employees
+@employees.route('/employees/', methods=['GET'])
+def get_all_employees():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from Employees')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
